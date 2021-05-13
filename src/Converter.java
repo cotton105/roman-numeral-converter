3218432992d73b084e3d3e9f16b0b555d5033d2e
cotton105 <lukecotton105@gmail.com>
2021-05-13 16:26:26 +0100

    Add tests.

--- src/Converter.java
+++ src/Converter.java
@@ -1,27 +1,50 @@
 import javax.swing.*;
-import java.util.Arrays;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Converter {
+    private static Map<Character, Integer> numeralMapping = new HashMap<>();
     JFrame frame;
+    // Specify values of Roman numerals in a map
+    static {
+        numeralMapping.put('I', 1);
+        numeralMapping.put('V', 5);
+        numeralMapping.put('X', 10);
+        numeralMapping.put('L', 50);
+        numeralMapping.put('C', 100);
+        numeralMapping.put('D', 500);
+        numeralMapping.put('M', 1000);
+        numeralMapping = Collections.unmodifiableMap(numeralMapping);
+    }
+
 
     public Converter() {
         frame = new window.ConverterWindow();
     }
 
-    public boolean validNumerals(String numerals) {
-        // TODO: Improve validity check
-        String[] romanCharSet = {"I", "V", "X", "L", "C", "D", "M"};
-        String[] numeralsArray = numerals.split("");
-        boolean valid = true;
-        if ( !isNumeric(numerals) ) {
-            for ( String c : numeralsArray ) {
-                if ( Arrays.stream(romanCharSet).noneMatch(c::equals) ) {
-                    valid = false;
-                    break;
-                }
-            }
+    public int romanToArabic(String numerals) {
+        int total = 0;
+        int chunk = 0;
+        for ( int i=1; i<numerals.length()-1; i++ ) {
+            int c = numeralMapping.get(numerals.charAt(i-1));
+            int nextC = numeralMapping.get(numerals.charAt(i));
+
         }
-        return valid;
+
+        return total;
+
+    }
+
+    public String arabicToRoman(int number) {
+        return null;
+    }
+
+    public boolean validNumerals(String numerals) {
+        String numeralRegex = "^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$";  // regex courtesy of user paxdiablo (stackoverflow)
+        Pattern pattern = Pattern.compile(numeralRegex, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(numerals);
+        return matcher.find();
     }
 
     private boolean isNumeric(String val) {
@@ -29,13 +52,12 @@ public class Converter {
             Integer.parseInt(val);
             return true;
         } catch ( NumberFormatException ex ) {
-            ex.printStackTrace();
             return false;
         }
     }
 
-
     public static void main(String[] args) {
         new Converter();
     }
+
 }
---
+++ tests/TestConverter.java
@@ -0,0 +1,43 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestConverter {
+    private final Converter converter = new Converter();
+    private final int[] conversionTestSetArabic = new int[] {
+            3, 3999, 2021, 1984,
+            39, 246, 789, 2421,
+            160, 207, 1009, 1066,
+            1776, 1918, 1954, 2014};
+    private final String[] conversionTestSetRoman = new String[] {
+            "III", "MMMCMXCIX", "MMXXI", "MCMLXXXIV",
+            "XXXIX", "CCXLVI", "DCCLXXXIX", "MMCDXXI",
+            "CLX", "CCVII", "MIX", "MLXVI",
+            "MDCCLXXVI", "MCMXVIII", "MCMLIV", "MMXIV"};
+    private final String[] invalidNumeralsTestSet = new String[] {
+            "IIII", "IXXMM",
+            "XXIXIX", "DCCXLXXIX",
+            "LCX", "IMX",
+            "MDCCXLXVI", "MCLMIV",
+            "TEST", "INVALID", "FAIL", "ROMAN"};
+
+    @Test
+    void testAccurateRomanToArabicConversion() {
+        for (int i=0; i<conversionTestSetRoman.length; i++)
+            assertEquals(conversionTestSetArabic[i], converter.romanToArabic(conversionTestSetRoman[i]));
+    }
+
+    @Test
+    void testAccurateArabicToRomanConversion() {
+        for (int i=0; i<conversionTestSetArabic.length; i++)
+            assertEquals(conversionTestSetRoman[i], converter.arabicToRoman(conversionTestSetArabic[i]));
+    }
+
+    @Test
+    void testValidRomanNumerals() {
+        for (int i=0; i<conversionTestSetRoman.length; i+=2)
+            assertTrue(converter.validNumerals(conversionTestSetRoman[i]));
+        for (String numerals : invalidNumeralsTestSet)
+            assertFalse(converter.validNumerals(numerals));
+    }
+}
